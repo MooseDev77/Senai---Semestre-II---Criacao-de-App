356e2b18c0894028e6326f4842f15205becc77be
Aluno Dendezeiros <aluno.den@senaibahia.com.br>
2025-09-08 15:06:44 -0300

    salvando att 4

---
+++ aula/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import InfoCliente from './Componentes/InfoCliente';
+
+function App() {
+  
+
+  return (
+    <>
+      <InfoCliente />
+    </>
+  )
+}
+
+export default App;
+
